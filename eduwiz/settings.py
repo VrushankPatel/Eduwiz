148e734972a4e6acd636d5a2427036424edb9ad9
Vrushank Patel <34762923+VrushankPatel@users.noreply.github.com>
2019-08-18 21:36:43 +0530

    Update settings.py

--- eduwiz/settings.py
+++ eduwiz/settings.py
@@ -7,8 +7,8 @@ ALLOWED_HOSTS = ['192.168.43.121', 'localhost']
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "serviceeduwiz@gmail.com"
-EMAIL_HOST_PASSWORD = "Vrushank@123"
+EMAIL_HOST_USER = "" # write email ID here
+EMAIL_HOST_PASSWORD = "" # Write Password here
 EMAIL_PORT = 587
 
 INSTALLED_APPS = [
